b459b5ffa09d39b67a8e1ff193c25e0cd06edda3
doctor1723 <doctor240290@mail.ru>
2024-07-23 18:59:58 +0300

    Add files via upload

---
+++ 'module 2_2'.py
@@ -0,0 +1,26 @@
+number=int(input('Введите число: '))
+first = 10
+second = 10
+third = 10
+if number == 10 :
+    print(first, second, third)
+number= int(input( 'Введите число :'))
+first= 14
+second= 9
+third= 14
+if number == 14 or number == 4 :
+    print(first, third)
+else:
+    print(second)
+number= int(input(' Введите число: '))
+first= 3
+second= 9
+third= 41
+if number == 2 and number == 4 :
+    print( first)
+elif number ==10 :
+    print( secod)
+elif number == 11:
+    print(third)
+else:
+    print(0)
